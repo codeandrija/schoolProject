b060d24db92ad4c54f3f48c0a7854f60c5da532c
codeandrija <codeandrija@gmail.com>
2023-05-26 20:50:16 +0200

    Commit 3

--- src/main/java/com/eazybytes/eazyschool/controller/HomeController.java
+++ src/main/java/com/eazybytes/eazyschool/controller/HomeController.java
@@ -1,13 +1,15 @@
 package com.eazybytes.eazyschool.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
-    @GetMapping(value = {"/home","","/"})
-    public String displayHomePage(){
-return "home.html";
+    @GetMapping(value = {"/home", "", "/"})
+    public String displayHomePage(Model model) {
+        model.addAttribute("username", "John Doe");
+        return "home.html";
     }
 
 
